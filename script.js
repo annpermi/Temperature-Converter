beb64985a1e5b933af5e83573616dc724f33a03d
annpermi <perminova.anna85@gmail.com>
2021-03-02 17:01:35 -0500

    added script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var celsius = -1;
+var fahrenheit = celsius * 9 / 5 + 32 + '\u00B0F';
+console.log(fahrenheit);
+
+var fahrenheit = 30;
+var celsius = (fahrenheit - 32) * 5/9 + '\u00B0C';
+console.log(celsius);
