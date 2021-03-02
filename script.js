a8950f361e60246bcb7ab4afb1a9601d77a924b4
annpermi <perminova.anna85@gmail.com>
2021-03-02 17:09:36 -0500

    changed script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var celsius = -1;
-var fahrenheit = celsius * 9 / 5 + 32 + '\u00B0F';
-console.log(fahrenheit);
+var fahrenheit = celsius * 9 / 5 + 32;
+console.log(celsius + '\u00B0C is ' + fahrenheit + '\u00B0F');
 
-var fahrenheit = 30;
-var celsius = (fahrenheit - 32) * 5/9 + '\u00B0C';
-console.log(celsius);
+var fahrenheit = 36;
+var celsius = (fahrenheit - 32) * 5/9;
+console.log(fahrenheit + '\u00B0F is ' + celsius + '\u00B0C');
